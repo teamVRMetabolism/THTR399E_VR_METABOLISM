12a54e9f688603a229d7c5e32c966b102b7dae50
Shum <robert.shum@ubc.ca>
2023-02-16 18:22:43 -0800

    testing vr with 3dm initial commit

--- Assets/Scripts/SceneData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewSceneData", menuName = "Scene Data", order = 51)]
-public class SceneData : ScriptableObject {
-    public HashSet<Animator> nodes;
-    public HashSet<Animator> edges;
-
-}
--- Assets/Scripts/ScriptableObjects/AnimContainer/AnimationContainer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewAnimationContainer", menuName = "Animation Container", order = 51)]
-public class AnimationContainer : ScriptableObject
-{
-    public List<AnimationClip> Animations;
-}
