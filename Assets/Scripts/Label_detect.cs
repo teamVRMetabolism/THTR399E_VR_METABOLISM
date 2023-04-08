b37c67599342525c4d652cc8a0eff042dbe75a00
jwang412s <jerrywang412s@gmail.com>
2023-04-07 17:35:11 -0700

    gamified fermentation

--- Assets/Scripts/Label_detect.cs
+++ Assets/Scripts/Label_detect.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class Label_detect : MonoBehaviour
 {
     public string correctTag; // set this in the Inspector window
+    public string correctTag2; // set this in the Inspector window
 
-    private void OnCollisionEnter(Collision other) {
-        Debug.Log("collided --------------------------------------------");
-        if (other.gameObject.tag == correctTag) {
-            // molecule with correct tag has entered the boxed area
-            Debug.Log("Correct molecule entered!");
-            // add your own code here to handle the correct molecule entering
-        }
+    void Start() {
+        
     }
 
-    private void OnTriggerExit(Collider other) {
-        if (other.gameObject.CompareTag(correctTag)) {
-            // molecule with correct tag has exited the boxed area
-            Debug.Log("Correct molecule exited.");
-            // add your own code here to handle the correct molecule exiting
+    void Update() {
+        
+    }
+
+    
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("collided --------------------------------------------");
+        if (other.gameObject.tag == correctTag || other.gameObject.tag == correctTag2) {
+            // molecule with correct tag has entered the boxed area
+            Debug.Log("Correct molecule entered!");
+
+            Transform collidedMolecule = other.gameObject.transform;
+            TESTING_0_Controller controller = GameObject.Find("SceneController").GetComponent<TESTING_0_Controller>();
+            if (controller.originalPositions.ContainsKey(correctTag))
+            {
+                collidedMolecule.GetComponent<Rigidbody>().isKinematic = true;
+                collidedMolecule.position = controller.originalPositions[correctTag];
+            }
         }
     }
 }
--- Assets/Scripts/TESTING_0_Controller.cs
+++ Assets/Scripts/TESTING_0_Controller.cs
@@ -8,7 +8,28 @@ public class TESTING_0_Controller : MonoBehaviour
     public SteamVR_Action_Boolean resetButton; 
     public SteamVR_Action_Boolean menuReturn;
     public GameObject player;
+    public List<Transform> randomPositions; // The list of positions to randomly assign to the objects
 
+    public Dictionary<string, Vector3> originalPositions = new Dictionary<string, Vector3>(); // A dictionary to store the original positions of the objects
+    private List<Vector3> usedPositions = new List<Vector3>(); // A list to store the positions that have already been used
+
+
+    void Start()
+    {
+        // Store the original positions of the objects
+        foreach (Transform child in transform)
+        {
+            originalPositions.Add(child.tag, child.transform.position);
+        }
+
+        // Randomly assign positions to the objects
+        foreach (Transform child in transform)
+        {
+            Vector3 randomPos = GetRandomPosition();
+            child.position = randomPos;
+            usedPositions.Add(randomPos);
+        }
+    }
     void Update()
     {
         if (resetButton.stateDown) {
@@ -21,6 +42,18 @@ public class TESTING_0_Controller : MonoBehaviour
         }
     }
 
+    private Vector3 GetRandomPosition()
+    {
+        Vector3 randomPos = randomPositions[Random.Range(0, randomPositions.Count)].position;
+
+        while (usedPositions.Contains(randomPos))
+        {
+            randomPos = randomPositions[Random.Range(0, randomPositions.Count)].position;
+        }
+
+        return randomPos;
+    }
+    
     void ResetScene() {
         Object[] objects = FindObjectsOfType(typeof(GameObject));
 
